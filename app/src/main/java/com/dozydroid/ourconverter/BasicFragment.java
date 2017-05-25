2540af32815abc23fc1716e20c7e7b2f266a4718
Muhammad Waqas <waqasazamkml@gmail.com>
2017-05-25 23:46:54 +0500

    Added FrameLayout in xml. Loaded child fragments inside fragment_basic.xml.

--- app/src/main/java/com/dozydroid/ourconverter/BasicFragment.java
+++ app/src/main/java/com/dozydroid/ourconverter/BasicFragment.java
@@ -2,15 +2,52 @@ package com.dozydroid.ourconverter;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 
 public class BasicFragment extends Fragment {
+    FragmentManager fragmentManager;
+    FrameLayout basicFragmentContainer;
+    LinearLayout layoutLength, layoutArea, layoutWeight, layoutVolume;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basic, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_basic, container, false);
+        basicFragmentContainer = (FrameLayout) v.findViewById(R.id.fragmentBasicContainer);
+        layoutLength = (LinearLayout) v.findViewById(R.id.layoutLength);
+        layoutArea = (LinearLayout) v.findViewById(R.id.layoutArea);
+        layoutWeight = (LinearLayout) v.findViewById(R.id.layoutWeight);
+        layoutVolume = (LinearLayout) v.findViewById(R.id.layoutVolume);
+        fragmentManager = getChildFragmentManager();
+        layoutLength.setBackgroundResource(R.drawable.selected_bg);
+        fragmentManager.beginTransaction().replace(R.id.fragmentBasicContainer, new BasicLengthFragment()).commit();
+        layoutLength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutLength.setBackgroundResource(R.drawable.selected_bg);
+                layoutArea.setBackgroundResource(R.drawable.deselected_bg);
+                layoutWeight.setBackgroundResource(R.drawable.deselected_bg);
+                layoutVolume.setBackgroundResource(R.drawable.deselected_bg);
+                fragmentManager.beginTransaction().replace(R.id.fragmentBasicContainer, new BasicLengthFragment()).commit();
+            }
+        });
+        layoutArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutArea.setBackgroundResource(R.drawable.selected_bg);
+                layoutLength.setBackgroundResource(R.drawable.deselected_bg);
+                layoutWeight.setBackgroundResource(R.drawable.deselected_bg);
+                layoutVolume.setBackgroundResource(R.drawable.deselected_bg);
+                fragmentManager.beginTransaction().replace(R.id.fragmentBasicContainer, new BasicAreaFragment()).commit();
+            }
+        });
+        return v;
     }}
