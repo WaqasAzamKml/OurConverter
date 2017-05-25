efe6f3a9a36412f8645c0e1a27f1c4e2191194ce
Muhammad Waqas <waqasazamkml@gmail.com>
2017-05-25 23:45:44 +0500

    UI Tweaks. Spinner made functional. Added TextWatcher on etOption

--- app/src/main/java/com/dozydroid/ourconverter/BasicLengthFragment.java
+++ app/src/main/java/com/dozydroid/ourconverter/BasicLengthFragment.java
@@ -2,9 +2,12 @@ package com.dozydroid.ourconverter;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ public class BasicLengthFragment extends Fragment {
     EditText etLength;
     Spinner spnOption;
     TextView tvCM;
+    String[] optionsArray = {"mm", "cm", "dm", "inch", "ft", "yd", "mile", "km", "nm"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +27,28 @@ public class BasicLengthFragment extends Fragment {
 
         etLength = (EditText) v.findViewById(R.id.etLength);
         spnOption = (Spinner) v.findViewById(R.id.spnOption);
+        tvCM = (TextView) v.findViewById(R.id.tvCM);
+
+        ArrayAdapter<String> optionsAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, optionsArray);
+        spnOption.setAdapter(optionsAdapter);
+
+        etLength.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvCM.setText(etLength.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return v;
     }
